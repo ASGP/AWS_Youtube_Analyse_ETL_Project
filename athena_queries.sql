78c6b72ec1bc4144fac3e2d61bd7768f4f206274
ASGP <punchiasgp@gmail.com>
2025-12-03 15:24:01 +0530

    Add query for top like/views ratio videos
    Query selects top videos based on like/views ratio.

---
+++ athena_queries.sql
@@ -0,0 +1,14 @@
+--1 Top Like/views ratio videos
+SELECT 
+    video_id,
+    title,
+    channel_title,
+    views,
+    likes,
+    (likes * 1.0 / NULLIF(views, 0)) AS like_rate,
+    country
+FROM processed
+WHERE views > 100000  
+ORDER BY like_rate DESC
+LIMIT 20;
+
